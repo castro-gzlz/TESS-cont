85dbc1bf9f9dc1afba08db2119be16f247c6dce6
Yolanda Frensch <yolandafrensch@gmail.com>
2026-05-06 10:50:42 +0100

    Add optional TACO output saving
    Introduced a 'TACO' flag in OPTIONAL (defaults to False if not provided). When enabled, creates dedicated output directories under 'output/TACO/' and saves the resampled data as CSVs.

--- TESS-cont.py
+++ TESS-cont.py
@@ -191,6 +191,11 @@ try:
     plot_target_name = OPTIONAL['plot_target_name'] == 'True'
 except:
     plot_target_name = False
+
+try:
+    TACO = OPTIONAL['TACO'] == 'True'
+except:
+    TACO = False
     
     
 #@|---------(HEATMAP arguments)-------------
@@ -555,6 +560,15 @@ CROWDSAP_pixel_by_pixel = resampled_list[idx_target] / resampled
 
 # In[ ]:
 
+#@|-----TACO----|
+if TACO:
+	if not os.path.isdir('output/TACO/'):
+		os.mkdir('output/TACO/')
+	if not os.path.isdir(f'output/TACO/{target_name}/'):
+		os.mkdir(f'output/TACO/{target_name}/')
+	pd.DataFrame(resampled).to_csv(f'output/TACO/{target_name}/{target_name}_S{sector}_{tpf_or_tesscut}_resampled.csv', index=False)
+	pd.DataFrame(resampled_list[idx_target]).to_csv(f'output/TACO/{target_name}/{target_name}_S{sector}_{tpf_or_tesscut}_resampled_list.csv', index=False)
+    
 
 #@|-----------------------------------------#@|
 #@|---We select/create the proper apeture---#@|
